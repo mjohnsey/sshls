d046002c3b696e344c2df9014552f83bf79e88ca
Michael Johnsey <mjohnsey@gmail.com>
2018-06-05 16:30:34 -0500

    Add a blacklist and remove cruft

--- cmd/root.go
+++ cmd/root.go
@@ -1,33 +1,14 @@
-// Copyright © 2017 Michael Johnsey <mjohnsey@gmail.com>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 package cmd
 
 import (
 	"fmt"
-	"os"
-	"github.com/pkg/errors"
 	"log"
+	"os"
 	"path/filepath"
-	"github.com/mikkeloscar/sshconfig"
 	"sort"
+
+	"github.com/mikkeloscar/sshconfig"
+	"github.com/pkg/errors"
 
 	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
@@ -42,8 +23,6 @@ type ByHost struct{ Hosts }
 
 func (s ByHost) Less(i, j int) bool { return s.Hosts[i].Host[0] < s.Hosts[j].Host[0] }
 
-var Interactive bool
-var PingOnly bool
 var hosts Hosts
 
 func (hosts Hosts) PrettyPrintStrings() []string {
@@ -54,18 +33,11 @@ func (hosts Hosts) PrettyPrintStrings() []string {
 	return result
 }
 
-func main(cmd *cobra.Command, args []string, isInteractive bool, onlyPing bool) {
+func main(cmd *cobra.Command, args []string) {
 	if hosts == nil {
 		log.Fatal("Could not get the hosts!")
 	}
-	if isInteractive {
-		fmt.Println("Interactive!")
-		if onlyPing{
-			fmt.Println("I will only ping not open an ssh session!")
-		}
-	} else {
-		printHosts(hosts)
-	}
+	printHosts(hosts)
 }
 
 func printHosts(hosts Hosts) {
@@ -79,7 +51,7 @@ var RootCmd = &cobra.Command{
 	Use:   "sshls",
 	Short: "This lists all your ssh hosts",
 	Run: func(cmd *cobra.Command, args []string) {
-		main(cmd, args, Interactive, PingOnly)
+		main(cmd, args)
 	},
 }
 
@@ -95,9 +67,6 @@ func Execute() {
 // Cobra command run on initialization
 func init() {
 	cobra.OnInitialize(getHosts)
-	RootCmd.Flags().BoolVarP(&Interactive, "interactive", "i", false, "Run this in interactive mode")
-	// This flag gets ignored unless you run in interactive mode (probably a better way of doing it but don't feel like figuring it out)
-	RootCmd.Flags().BoolVarP(&PingOnly, "ping", "p", false, "Only run with ping when you select a host")
 }
 
 // This will get the ssh config file and set the objects
@@ -110,8 +79,21 @@ func getHosts() {
 	// Grab the ssh config and parse it
 	sshConfigFile := filepath.Join(home, ".ssh", "config")
 	theHosts, err := sshconfig.ParseSSHConfig(sshConfigFile)
+
+	whitelist := make([]*sshconfig.SSHHost, 0)
+
+	blacklist := map[string]bool{
+		"*": false,
+	}
+
+	for _, host := range theHosts {
+		if _, ok := blacklist[host.Host[0]]; !ok {
+			whitelist = append(whitelist, host)
+		}
+	}
+
 	// sort by host name
-	sort.Sort(ByHost{theHosts})
+	sort.Sort(ByHost{whitelist})
 	// set the hosts
-	hosts = theHosts
+	hosts = whitelist
 }
