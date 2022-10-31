43545f8ebf1bb7888bd1e3cb61d1ef8cb8aae7b7
Michael Johnsey <michaeljohnsey@changeresearch.com>
2022-10-31 18:07:46 -0500

    Add user to pretty string

--- lib/hosts.go
+++ lib/hosts.go
@@ -19,7 +19,7 @@ func (s ByHost) Less(i, j int) bool { return s.Hosts[i].Host[0] < s.Hosts[j].Hos
 func (hosts Hosts) PrettyPrintStrings() []string {
 	result := make([]string, len(hosts))
 	for i, host := range hosts {
-		result[i] = fmt.Sprintf("%s - %s\n", host.Host, host.HostName)
+		result[i] = fmt.Sprintf("%s - %s (%s)\n", host.Host[0], host.HostName, host.User)
 	}
 	return result
 }
