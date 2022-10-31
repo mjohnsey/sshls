636231cb11d41e1ad89c5dff7a6a70cc3cadab9f
Michael Johnsey <mjohnsey@gmail.com>
2022-10-31 18:28:34 -0500

    Add user to output (#3)
    * Add user to pretty string
    
    * Add json version

--- lib/hosts.go
+++ lib/hosts.go
@@ -19,19 +19,19 @@ func (s ByHost) Less(i, j int) bool { return s.Hosts[i].Host[0] < s.Hosts[j].Hos
 func (hosts Hosts) PrettyPrintStrings() []string {
 	result := make([]string, len(hosts))
 	for i, host := range hosts {
-		result[i] = fmt.Sprintf("%s - %s\n", host.Host, host.HostName)
+		result[i] = fmt.Sprintf("%s - %s (%s)\n", host.Host[0], host.HostName, host.User)
 	}
 	return result
 }
 
 func (hosts Hosts) AsJsonString() *string {
-	hostMap := make(map[string]string)
+	result := make(map[string]map[string]string)
 	for _, host := range hosts {
-		k := &host.Host[0]
-		v := &host.HostName
-		hostMap[*k] = *v
+		result[host.Host[0]] = make(map[string]string)
+		result[host.Host[0]]["hostname"] = host.HostName
+		result[host.Host[0]]["user"] = host.User
 	}
-	hostMapJsonB, _ := json.Marshal(hostMap)
-	asStr := string(hostMapJsonB)
+	jsonString, _ := json.Marshal(result)
+	asStr := string(jsonString)
 	return &asStr
 }
